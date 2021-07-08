8eeefaf2de6f0abb990d6536eb1de52bd7edc542
Jose Lopes <946393361bill@gmail.com>
2021-07-08 21:50:19 +0100

    estilizando Home

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Button } from './components/Button';
-import './services/firebase'
+
+import './services/firebase';
+
+import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Button/>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,34 @@
 import illustrationImg from '../assets/images/illustration.svg';
-
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
 
 export function Home() {
     return(
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie Salas de Q&amp;A ao Vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo real!</p>
             </aside>
+            <main>
+              <div>
+                <img src={logoImg} alt="Letmeask" />
+                  <button>
+                    <img src={googleIconImg} alt="Logo do google" />
+                    Crie sua sala com o google
+                    </button>
+                    <div>Ou entre em uma sala</div>
+                    <form>
+                      <input
+                        type="text"
+                        placeholder="Digite o código da sala"
+                        />
+                    </form>
+                    <button type="submit">
+                      Entrar na Sala
+                    </button>
+                </div>
+            </main>
         </div>
     );
 };
