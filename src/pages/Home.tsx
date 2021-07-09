41a0ce3a00ba69bf3d8a2256584223b0e6add292
Jose Lopes <946393361bill@gmail.com>
2021-07-09 01:03:07 +0100

    componente Home finalizada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,31 +2,39 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import { Button } from '../components/Button';
+
+
+import '../styles/auth.scss'
+
 export function Home() {
     return(
         <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie Salas de Q&amp;A ao Vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo real!</p>
+                <strong>Crie Salas de Q&amp;A ao Vivo!</strong>
+                <p>Tire as dúvidas da sua audiência em tempo real.</p>
             </aside>
             <main>
-              <div>
+              <div className="main-content">
                 <img src={logoImg} alt="Letmeask" />
-                  <button>
+                  <button className="create-room-google">
                     <img src={googleIconImg} alt="Logo do google" />
                     Crie sua sala com o google
                     </button>
-                    <div>Ou entre em uma sala</div>
+                    <button className="create-room-facebook">
+                    Crie sua sala com o facebook
+                    </button>
+                    <div className="separator">Ou entre em uma sala</div>
                     <form>
                       <input
                         type="text"
                         placeholder="Digite o código da sala"
                         />
                     </form>
-                    <button type="submit">
+                    <Button type="submit">
                       Entrar na Sala
-                    </button>
+                    </Button>
                 </div>
             </main>
         </div>
