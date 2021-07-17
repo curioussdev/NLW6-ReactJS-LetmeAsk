cfca60e497c272146af4e15b2b9748ecbb094865
Jose Lopes <946393361bill@gmail.com>
2021-07-17 22:32:05 +0100

    comentando os hooks da NewRoom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../contexts/AuthContext';
-import {useContext} from 'react';
+// import { AuthContext } from '../contexts/AuthContext';
+// import {useContext} from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -12,7 +12,7 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss'
 
 export function NewRoom() {
-    const {user} = useContext(AuthContext)
+    // const {user} = useContext(AuthContext)
 
     return(
         <div id="page-auth">
@@ -24,7 +24,7 @@ export function NewRoom() {
             <main>
              <div className="main-content">
                 <img src={logoImg} alt="Letmeask" />
-                 <h1>{user?.name}</h1>
+                 
                     <h2>Criar uma nova sala</h2>
                     <form>
                       <input
