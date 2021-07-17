e1da8dae9ef0b1f5bfe6cb071266003f8c398144
Jose Lopes <946393361bill@gmail.com>
2021-07-17 21:43:07 +0100

    mantendo estado de usuário logado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from 'react';
+import {createContext, useState, useEffect} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import { Home } from "./pages/Home";
@@ -21,6 +21,25 @@ export const AuthContext = createContext({} as AuthContextType);
 
 export function App() {
   const [user, setUser] = useState<user>();
+  
+  useEffect(() => {
+    auth.onAuthStateChanged( user => {
+      if (user) {
+        
+        const {displayName, photoURL, uid} = user;
+        
+        if(!displayName || !photoURL) {
+          throw new Error('Missing Information from Google account.');
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        });
+      }
+    })
+  }, [])
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
