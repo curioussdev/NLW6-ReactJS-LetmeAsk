24bb7a869dbf24b30ac6acb1a34e2bf3477b3b79
Jose Lopes <946393361bill@gmail.com>
2021-07-18 19:55:38 +0100

    criando hook de autenticação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,10 +9,11 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 import '../styles/auth.scss'
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
     const history = useHistory();
-    const {user, signInWithGoogle} = useContext(AuthContext);
+    const {user, signInWithGoogle} = useAuth()
 
    
     async function handleCreateRoom() { // usando o hook useHistorry do react-router-dom, navegamos entre as páginas
